d8db5511b11661eb9101f9e847eb88110e129279
Jenny Yi <yi.404@osu.edu>
2021-03-06 00:58:30 -0500

    Update force sensor random message

--- ForceSensorRandomMessage/ForceSensorRandomMessage.ino
+++ ForceSensorRandomMessage/ForceSensorRandomMessage.ino
@@ -1,16 +1,23 @@
 
 #include <LiquidCrystal.h>
 #define fsrpin A0
+#define fsrpin2 A1
 
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-int fsrreading;
+int soapFsrreading;
+int doorFsrreading;
 
 int timer = 500;
+bool soapPressed = false;
+int timePressed = 0;
 
-String messages[] = {"Message", "Test"}; 
-int messagesLength = 2;
+String goodMessages[] = {"Message", "Test"}; 
+int goodMessagesLength = 2;
+
+String badMessages[] = {"Message", "Test"}; 
+int badMessagesLength = 2;
 
 
 void setup() {
@@ -20,18 +27,39 @@ void setup() {
 
 void loop() {
 
-  fsrreading = analogRead(fsrpin);
+  soapFsrreading = analogRead(fsrpin);
+  doorFsrreading = analogRead(fsrpin2);
+  
   lcd.home();
 
-  if (fsrreading > 500) {
-    printMessage();
+  if (soapPressed && (millis() - timePressed > 30000)) {
+    soapPressed = false; 
+  }
+
+  if (soapFsrreading > 500) {
+    soapPressed = true;
+    timePressed = millis();
+  }
+  
+  if (doorFsrreading > 500 && soapPressed) {
+    printMessage("Good");
     delay(timer);
     lcd.clear();
-  } 
+  } else if (doorFsrreading > 500 && !soapPressed) {
+    printMessage("Bad");
+    delay(timer);
+    lcd.clear();
+  }
 }
 
-void printMessage() {
-  randIndex = random(0, messagesLength);
-  lcd.print(messages[randIndex]);
+void printMessage(String mode) {
+  if (mode == "Good") {
+  int randIndex = random(0, goodMessagesLength);
+  lcd.print(goodMessages[randIndex]);
+  } else {
+  int randIndex = random(0, badMessagesLength);
+  lcd.print(badMessages[randIndex]);
+  }
+  
   
 }
